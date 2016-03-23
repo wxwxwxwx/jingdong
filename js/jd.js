5ddf7748093a21cf2251b5f1d99c66c8e41a17a2
王昕 <97677613@qq.com>
2016-03-23 11:53:33 +0800

    ...

---
+++ js/jd.js
@@ -0,0 +1,51 @@
+window.onload=function(){
+
+// 轮播图开始
+var win=allgain(".slider-main")[0]
+var img=allgain("a",win)
+var wins=allgain(".slider")[0]
+var li=allgain("li",wins)
+var wins=allgain(".btn")[0]
+var btn=allgain("div",wins)
+console.log(li)
+console.log(img)
+    carousel(win,li,img,btn,2000,500,"hot","slider-item")
+// var num=0;
+//  li[0].className="hot"
+//  img[0].style.zIndex=10000000
+//  var t=setInterval(move,1000)
+// win.onmouseover=function(){
+//     clearInterval(t)
+// }
+// win.onmouseout=function(){
+//     clearInterval(t)
+//     t=setInterval(move,1000)
+// }
+// for(var i=0;i<img.length;i++){
+//     li[i].index=i;
+// li[i].onclick=function(){
+//     num=this.index
+//     for(var i=0;i<img.length;i++){
+//     img[i].style.zIndex=0
+//     li[i].className="slider-item"
+// }
+//     img[this.index].style["z-index"]=3
+//     li[this.index].className="hot"
+// }
+// }
+
+// function move(){
+// num++;
+// if(num==img.length){
+//     num=0
+// }
+// for(var i=0;i<img.length;i++){
+//     img[i].style.zIndex=0
+//     li[i].className="slider-item"
+// }
+// img[num].style.zIndex=3
+// li[num].className="hot"
+// // console.log(getComputedStyle(li[num],null).className)
+// }
+// // 轮播图结束
+}
